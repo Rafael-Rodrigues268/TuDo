6213d4a2fb4c95154f210f428445ee2c7f83b567
Rafael-Rodrigues268 <rafaelrja2006@gmail.com>
2025-11-18 11:55:00 -0300

    aula_tudo
    html,urls,django,e mais um pouco de tudo

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main.models import Task
 
+#admin.site.register(Task)
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('titulo','descricao','usuario')
+    list_filter = ('concluida', 'prioridade')
+    search_fields =('titulo',)
